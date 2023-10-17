01efd989c91a33998404d83d8c47cc8cf7043233
ngmtuandev <nguyenmanhtuancomputer@gmail.com>
2023-10-17 10:45:51 +0700

    finish api backend vocabulary version 1 not progress

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 const userRouter = require("./user");
+const vocabularyRouter = require("./vocabulary");
 
 const initRoute = (app) => {
   app.use("/api/user", userRouter);
+  app.use("/api/vocabulary", vocabularyRouter);
 };
 
 module.exports = initRoute;
